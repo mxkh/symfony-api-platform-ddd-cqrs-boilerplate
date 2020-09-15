7d24aa1946c777df276b66bff004df98191e6cd0
Maksym Khartanovych <m.khartanovych@gmail.com>
2020-09-15 10:24:37 +0200

    Add change email use case

---
+++ src/Domain/User/Event/UserEmailChanged.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Acme\Domain\User\Event;
+
+use Acme\Domain\Shared\ValueObject\DateTime;
+use Acme\Domain\User\ValueObject\Email;
+use Ramsey\Uuid\UuidInterface;
+
+final class UserEmailChanged
+{
+    public UuidInterface $uuid;
+
+    public Email $email;
+
+    public DateTime $updatedAt;
+
+    public function __construct(UuidInterface $uuid, Email $email, DateTime $updatedAt)
+    {
+        $this->email = $email;
+        $this->uuid = $uuid;
+        $this->updatedAt = $updatedAt;
+    }
+}
